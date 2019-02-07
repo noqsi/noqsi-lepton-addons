8b173e359a3866c47aab5a022fe5a48c25f639de
John Doty <jpd@noqsi.com>
2019-02-07 18:12:57 +0000

    GPL boilerplate

--- tsv/src/tsv.scm
+++ tsv/src/tsv.scm
@@ -1,3 +1,22 @@
+;;; tsv.scm - Module for Lepton EDA
+;;; Read a tab separated value file.
+;;;
+;;; Copyright (C)  2019 John P. Doty
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 (define-module (noqsi tsv))
 
 (use-modules (ice-9 rdelim))
