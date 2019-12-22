28643cce6466e6875e95be101a776f6efe8043d6
John Doty <jpd@noqsi.com>
2019-12-22 18:55:10 -0500

    Add scripts that hadn't been controlled

---
+++ check-duplicates/src/gnet-check-duplicates.scm
@@ -0,0 +1,97 @@
+;;; gEDA - GPL Electronic Design Automation
+;;; gnet-check-duplicates.scm - check for duplicate refdes-pinnumber pairs.
+;;; Copyright (C)  2015 John P. Doty
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+(use-modules (srfi srfi-1))
+
+;; Run the stuff below and issue a diagnostic if any duplicates are found.
+;; This exits gnetlist with a status of 1 if it has detected an error.
+;; The only output is to (current-error-port).
+;;
+(define (check-duplicates unused-filename)
+	(let 
+		(
+			(dups (find-duplicates (every-connection)))
+		)
+		(if (not (null? dups))
+			(begin
+				(format (current-error-port) 
+					"Duplicate refdes-pin combinations:\n    ~A\n"
+					dups
+				)
+				(primitive-exit 1)
+			)
+		)
+	)
+)
+
+;; Make a sorted list of every connection to non-graphical symbols in the design.
+;; The connections are coded as text in the form refdes-pin.
+;;
+(define (every-connection)
+	
+	(sort
+		(map refdes-pin
+			(apply append (map pins-on-package packages))
+		)
+		string<
+	)
+)
+
+;; Make a list of (refdes pin) for each pin on the "package" identified by refdes.
+;; Utilizes the fact that (get-pins-nets) does not remove duplicates.
+;;
+(define (pins-on-package refdes)
+
+	(map 
+		(lambda (pn)
+			(list refdes (car pn))
+		)
+		(get-pins-nets refdes)
+	)
+)
+
+;; Make a refdes-pin string from a (refdes pin) list.
+;; 
+(define (refdes-pin rp)
+
+	(string-append (car rp) "-" (cadr rp))
+)
+
+;; Make a list of duplicates from a sorted list
+;; This works by zipping a list with itself with the first object dropped,
+;; creating a list of pairs of neighbors.
+;; It filters that list to find pairs of (equal?) objects.
+;; It does not remove duplicated duplicates from the result.
+;;
+(define (find-duplicates l)
+
+	(if 
+		(null? l)
+		; then do nothing gracefully
+		'()
+		; else 
+		(unzip1
+			(filter 
+				(lambda (x)
+					(apply equal? x)
+				)
+				(zip l (cdr l))
+			)
+		)
+	)
+)
---
+++ pincount/src/pincount.scm
@@ -0,0 +1,146 @@
+;;; pincount.scm - Module for Lepton EDA
+;;; Determine the pin coount of a package
+;;;
+;;; Copyright (C)  2019 John P. Doty
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+(define-module noqsi pincount)
+
+(use-modules (srfi srfi-1))
+(use-modules (ice-9 regex))
+(use-modules (ice-9 format))
+(use-modules (noqsi tsv))
+
+(define counts (make-hash-table))
+
+(define (table-error message entry)
+	(format (current-error-port) "~A\n~S\n" message entry)
+	(primitive-exit 1)
+)
+
+(define num (make-regexp "^[0-9]+$"))
+
+(define (enter-pincount-for-footprint entry)
+	
+	(if (< (length entry) 2)
+		(table-error 
+			"A pincount table entry needs two fields."
+			entry
+		)
+	)
+	
+	(if 	(not (or 
+			(regexp-exec num (cadr entry))
+			(equal? (cadr entry) "*")
+		))
+		(table-error 
+			"Pincount must be number or \"*\"."
+			entry
+		)
+	)
+	
+	(hash-set! 
+		counts 
+		(car entry) 
+		(or (string->number (cadr entry)) (cadr entry))
+	)
+)
+
+(for-each enter-pincount-for-footprint (tsv-data "pincounts"))
+
+;; Constructor for list of prefixes and counts
+;;
+(define footprint-pins-list '())
+(define (footprint-pins-prefix . x)
+	(set! footprint-pins-list (cons x footprint-pins-list)))
+
+;; search the list
+;;
+(define (find-footprint-pins attrib)
+	(find (lambda (x) (string-prefix? (car x) attrib)) footprint-pins-list))
+
+
+;; Extract the pin count from the footprint with the prefix string removed.
+(define pin-count-regexp (make-regexp "[0-9]*"))
+(define (get-count-from-attrib pre attrib)
+	(let ((s (match:substring (regexp-exec pin-count-regexp attrib (string-length pre)))))
+		(if (string-null? s) #f s)))
+
+;; Look up the prefix and use it to extract the count as a character string.
+;; Recursively trims the footprint name f until it matches a prefix.
+;; Returns (prefix.count).
+;; Count could be "*".
+;; Return #f on failure.
+(define (prefix-pins f)
+	(if 
+		(string-null? f) 
+		#f 
+		(let
+			((pc (hash-ref counts f)))
+			(if pc 
+				(cons f pc)
+				(prefix-pins (string-drop-right f 1))
+			)
+		)
+	)
+)
+
+(define (pins-from-footprint f)
+	(let ((pc (prefix-pins f)))
+		(and
+			pc
+			(if (equal? (cdr pc) "*")
+				(get-count-from-attrib (car pc) f)
+				(cdr pc)
+			)
+		)
+	)
+)
+
+
+
+(define (get-numeric-attribute refdes name)
+	(let ((value (unique-attribute refdes name (get-all-package-attributes refdes name))))
+		(and 
+			value
+			(if (regex-exec num value)
+				(string->number value)
+				(numeric-warning refdes name value)
+			)
+		)
+	)
+)
+
+(define (numeric-warning refdes name value)
+	(format (current-error-port)
+		"For ~A ~A = ~A is not numeric"
+	)
+	#f
+)
+		 
+
+;; First try to get the pincount from a pins= attribute.
+;; If that fails, try to get it from the footprint.
+;; If that fails, resort to counting connections.
+(define (get-package-pincount p)
+	(or 
+		(get-numeric-attribute p "pins") 
+		(pins-from-footprint (get-package-attribute-f p "footprint"))
+		(length (get-pins p))
+	)
+)
+
+(export get-package-pincount enter-pincount-for-footprint pins-from-footprint)
